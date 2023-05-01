f675da909d8ebe9f8653231966c700123005a8b3
Lars Ratovo <lars.ratovo@gmail.com>
2023-05-01 05:45:05 +0300

    done

--- index.js
+++ index.js
@@ -1,37 +1,62 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const dns= require('dns');
+const mongoose=require('mongoose');
+const mongo=mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// Basic Configuration
+const port = process.env.PORT || 3000;
+const schema=new mongoose.Schema({
+  url:String
+});
+const Url=mongoose.model("url",schema);
+app.use(cors());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get("/api/whoami",function(req,res){
-  res.json({
-    ipaddress:req.ip,
-    language:req.headers['accept-language'],
-    software:req.headers['user-agent']
+app.post('/api/shorturl',(req,res)=>{
+    let url=req.body.url;
+    if(url.endsWith("/")){
+      url=url.substring(0,url.length-1);
+    }
+    dns.lookup(url,(error,address,family)=>{
+      if(error){
+        res.json({error:'invalid url'});
+        res.json({error:'Error Saved'});
+      }
+      let simp=new Url({url:url});
+      simp.save().then(simple=>{
+          res.json({original_url:simple.url,short_url:simple._id});
+      }).catch(err=>{
+        console.log(err);
+        res.json({error:err});
+      });
+    });
+});
+
+app.get('/api/shorturl/:id',(req,res)=>{
+  let id=req.params.id;
+  Url.findById(id).then(url=>{
+    console.log(url);
+  res.redirect("http://"+url.url);
+  }).catch(error=>{
+    console.log(error);
   });
 });
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
